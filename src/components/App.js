da95aa63701b5817aa4a933defc632e905f2c4d8
asionsolver <asionsolver274@gmail.com>
2024-01-12 12:29:14 +0600

    change app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import "../styles/Global.css";
 import Layout from "./Layout";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-// import Quiz from "./pages/Quiz";
-// import Login from './pages/Login';
-// import Home from './pages/Home';
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Layout>
-      {/* <Home /> */}
-      {/* <Signup/> */}
-      {/* <Login/> */}
-      {/* <Quiz /> */}
+      <Home />
+      <Signup/>
+      <Login/>
+      <Quiz />
       <Result/>
     </Layout>
   );
 }
+
 
 export default App;
